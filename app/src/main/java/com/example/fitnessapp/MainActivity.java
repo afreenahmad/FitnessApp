e0cddb05499f28f6b955fb554dc9a1521413acf5
afreenahmad <afreenahmad01@gmail.com>
2023-05-01 23:19:19 -0400

    error with inverting colors

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.fitnessapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -12,6 +15,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.example.fitnessapp.ViewHolder.WorkoutListAdapter;
 import com.example.fitnessapp.ViewHolder.WorkoutListHolder;
 import com.example.fitnessapp.db.workoutDatabase;
@@ -20,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     private int workout_id;
     private boolean filtered = false;
     private WorkoutViewModel WorkoutViewModel;
+    private String mCardScheme;
+
+    private WorkoutListAdapter adapter;
+    private RecyclerView recyclerView;
 
 
 
@@ -29,10 +38,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setSupportActionBar(findViewById(R.id.toolbar));
 
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        mCardScheme = sharedPref.getString("cardScheme","");
+
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        WorkoutListAdapter adapter = new WorkoutListAdapter(this);
+       // WorkoutListAdapter adapter = new WorkoutListAdapter(this);
+        adapter = new WorkoutListAdapter(this);
         recyclerView.setAdapter(adapter);
+        //adapter.setLightOrDark(mCardScheme);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -60,17 +75,50 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                RecyclerView recyclerView = findViewById(R.id.recyclerView);
-                WorkoutListAdapter adapter = new WorkoutListAdapter(this);
-                recyclerView.setAdapter(adapter);
-                WorkoutViewModel = new ViewModelProvider(this).get(WorkoutViewModel.class);
+                //RecyclerView recyclerView = findViewById(R.id.recyclerView);
+               // WorkoutListAdapter adapter = new WorkoutListAdapter(this);
+                //recyclerView.setAdapter(adapter);
+                //WorkoutViewModel = new ViewModelProvider(this).get(WorkoutViewModel.class);
                 WorkoutViewModel.filterWorkouts(filtered);
-                WorkoutViewModel.getAllWorkouts().observe(this, adapter::setWorkouts);
+               // WorkoutViewModel.getAllWorkouts().observe(this, adapter::setWorkouts);
 
             default:
                 return super.onOptionsItemSelected(item);
         }
 
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Get the value of the boolean from shared preferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int color = (prefs.getString("colorScheme", "").equals("light")? Color.WHITE:Color.DKGRAY);
+
+        String cardScheme = null;
+        if(prefs.getString("cardScheme","").equals("Light")){
+            cardScheme = "Light";
+        }else if(prefs.getString("cardScheme","").equals("Dark")){
+            cardScheme = "Dark";
+        }else{
+            cardScheme = "Inverted";
+
+        }
+
+
+
+        recyclerView.setBackgroundColor(color);
+
+        // If the value has changed, update the adapter
+        //if(!cardScheme.equals(mCardScheme)){
+            mCardScheme = cardScheme;
+            adapter.setLightOrDark(cardScheme);
+            adapter.notifyDataSetChanged();
+       // }
 
     }
 }
--- app/src/main/java/com/example/fitnessapp/ViewHolder/WorkoutListAdapter.java
+++ app/src/main/java/com/example/fitnessapp/ViewHolder/WorkoutListAdapter.java
@@ -16,13 +16,18 @@ import com.example.fitnessapp.db.workout;
 
 
 import java.util.List;
+import java.util.Objects;
 
 public class WorkoutListAdapter extends RecyclerView.Adapter <WorkoutListHolder> {
     public final LayoutInflater layoutInflater;
+    private Context mContext;
 
     private List<workout> workouts; // Cached copy of jokes
+    private int colorResId;
+    private String mCardScheme;
 
     public WorkoutListAdapter(Context context){
+        mContext = context;
 
         layoutInflater = LayoutInflater.from(context);
     }
@@ -34,6 +39,8 @@ public class WorkoutListAdapter extends RecyclerView.Adapter <WorkoutListHolder>
                 .inflate(R.layout.list_items, parent, false);
         return new WorkoutListHolder(itemView);
     }
+    public void setLightOrDark(String cardScheme){
+        mCardScheme = cardScheme;}
 
     @Override
     public void onBindViewHolder( WorkoutListHolder holder, int position) {
@@ -43,6 +50,32 @@ public class WorkoutListAdapter extends RecyclerView.Adapter <WorkoutListHolder>
             holder.nameView.setText(current.name);
             holder.muscleGroup.setText(current.muscleGroup);
             holder.description.setText(current.description);
+
+            if (Objects.equals(mCardScheme, "Light")) {
+                holder.nameView.setTextColor(Color.DKGRAY );
+                holder.muscleGroup.setTextColor(Color.DKGRAY );
+                holder.description.setTextColor(Color.DKGRAY);
+            } else if (Objects.equals(mCardScheme, "Dark")) {
+                holder.nameView.setTextColor(Color.LTGRAY);
+                holder.muscleGroup.setTextColor(Color.LTGRAY);
+                holder.description.setTextColor(Color.LTGRAY);
+            }
+
+            colorResId = Objects.equals(mCardScheme, "Light") ? "Biceps".equals(current.muscleGroup)
+                    ? R.color.colorLP
+                    : "Triceps".equals(current.muscleGroup) ? R.color.colorMP
+                    : "Glutes".equals(current.muscleGroup) ? R.color.colorHP : R.color.colorDefault
+                    : Objects.equals(mCardScheme, "Dark")
+                    ? "Triceps".equals(current.muscleGroup) ? R.color.colorLPDark
+                    : "Biceps".equals(current.muscleGroup) ? R.color.colorMPDark
+                    : "Glutes".equals(current.muscleGroup) ? R.color.colorHPDark : R.color.colorDefault
+                    : "Biceps".equals(current.muscleGroup) ? R.color.colorLPDarkInverted
+                    : "Triceps".equals(current.muscleGroup) ? R.color.colorMPDarkInverted
+                    : "Glutes".equals(current.muscleGroup) ? R.color.colorHPDarkInverted : R.color.colorDefault;
+
+
+            int color = ContextCompat.getColor(holder.itemView.getContext(), colorResId);
+            holder.cardView.setCardBackgroundColor(color);
 
 
 
